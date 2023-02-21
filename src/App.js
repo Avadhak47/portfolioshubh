7557e6056d178e73fbdba84ec7cfa42563886bd8
Shubham <99029472+Shubh404-SE@users.noreply.github.com>
2023-02-22 00:33:48 +0530

    Contact page done, now working on footer

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Navbar from './components/Navbar';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './components/Home';
 import About from './components/About';
+import Contact from './components/Contact';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <Routes>
         <Route exact path='/' element={<Home />} />
         <Route exact path="/about" element={<About/>} />
+        <Route exact path='/contactme' element={<Contact />} />
       </Routes>
       
     </Router>
