f1c156ce22922d679984e4c32c3ef4251e35946f
Shubham <99029472+Shubh404-SE@users.noreply.github.com>
2023-02-21 20:49:13 +0530

    navbar component created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
+import './style.css/Navbar.css';
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route exact path='/' element={""} />
+      </Routes>
       <h1>Shubham Tanwar</h1>
-    </div>
+    </Router>
   );
 }
 
---
+++ src/components/Navbar.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import { Link } from 'react-router-dom';
+
+function Navbar() {
+
+
+    window.addEventListener('scroll', (event) => {
+        let navbar = document.getElementsByClassName("navbar");
+
+        if (window.pageYOffset !== undefined) {
+            if (window.pageYOffset > 25) {
+                navbar[0].style.background = "#333333";
+            }
+            else {
+                navbar[0].style.background = "transparent";
+            }
+
+        }
+    })
+
+
+    return (
+        <div className='navbar'>
+            <input id="menu__toggle" type="checkbox" />
+            <label className="menu__btn" htmlFor="menu__toggle">
+                <span></span>
+            </label>
+            <img src="https://www.zarla.com/images/zarla-s-1x1-2400x2400-20211119-wwrwkmmjcm7hh3wfkvcc.png?crop=1:1,smart&width=250&dpr=2" alt="namelogo" />
+            <h3>Shubham Tanwar</h3>
+            <div className='navlinks'>
+                <Link to="/" className="link">Home</Link>
+                <Link to="/" className="link">About</Link>
+                <Link to="/" className="link">Skills</Link>
+                <Link to="/" className="link">Services</Link>
+                <Link to="/" className="link">Education</Link>
+                <Link to="/" className="link">Services</Link>
+                <Link to="/" className='link'>Project Details</Link>
+                <div className="icon">
+                    <a href="/"><GitHubIcon /></a>
+                </div>
+            </div>
+        </div>
+    )
+}
+export default Navbar;
