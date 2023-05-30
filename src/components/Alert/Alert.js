026e1a5b3c4caf1151b5572388b1d97b253ee12c
Shubham <99029472+Shubh404-SE@users.noreply.github.com>
2023-05-30 11:38:30 +0530

    bug XD

--- src/components/Alert/Alert.js
+++ src/components/Alert/Alert.js
@@ -2,7 +2,7 @@ import "./Alert.css";
 export default function Alert({alert, showaltr}) {
     const [name, email] = alert;
     return (
-        <div className="container" >
+        showaltr &&  <div className="container" >
             <div className={`alert-success`} role="alert">
                 <strong style={{color:"blueviolet"}}> ***Message Sent*** </strong> <br /> Thanks to approach Me, {name}. <br /> ASAP I'll reply you on {email}.</div>
         </div>
